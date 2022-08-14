5583bb71ce86a2f0a7e98d42060a8800fc41c3a6
mohammedboreny <Mohammed.boreny@gmail.com>
2022-08-14 21:41:06 +0300

    more Adjustments

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let employee = function (employeeID, fName, department, level, imageUrl) {
     this.department = department;
     this.level = level;
     this.imageUrl = imageUrl;
-    
-    this.salary = function (level) {
+  
+    this.salary = function () {
         if (this.level == "Junior") {
             min = Math.ceil(500);
             max = Math.floor(1000);
@@ -21,16 +21,18 @@ let employee = function (employeeID, fName, department, level, imageUrl) {
         }
         return Math.floor(Math.random() * (max - min) + min);
     };
-    return employeeID, fName, department, level, imageUrl, salary;
+    console.log(employeeID, fName, department, level, imageUrl, this.salary());
+    return 0;
 };
 function render(){
-    let person1 = employee(1000, "ghazi", "administration", "Senior", "");
-    let person2 = employee(1001, "lana", "Finance", "Senior", "");
-    let person3 = employee(1002, "tamara", "Markting", "Senior", "");
-    let person4 = employee(1003, "Saif", "administration", "Mid-Senior", "");
-    let person5 = employee(1004, "Omar", "Development", "Senior", "");
-    let person6 = employee(1005, "Rana", "Development", "Junior", "");
-    let person7 = employee(1006, "Hadi", "Finance", "Mid-Senior", "");
+    let person1 = employee("1000", "ghazi", "administration", "Senior", "");
+    let person2 = employee("1001", "lana", "Finance", "Senior", "");
+    let person3 = employee("1002", "tamara", "Markting", "Senior", "");
+    let person4 = employee("1003", "Saif", "administration", "Mid-Senior", "");
+    let person5 = employee("1004", "Omar", "Development", "Senior", "");
+    let person6 = employee("1005", "Rana", "Development", "Junior", "");
+    let person7 = employee("1006", "Hadi", "Finance", "Mid-Senior", "");
+    return "";
 }
 console.log(render());
 
