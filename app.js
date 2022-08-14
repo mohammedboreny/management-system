e325623cad03dfcfc12807ec0f18a3945404155a
mohammedboreny <Mohammed.boreny@gmail.com>
2022-08-14 19:53:31 +0300

    more Adjustments

---
+++ app.js
@@ -0,0 +1,9 @@
+function constructor(){
+
+    this.employee = employee;
+    this.fName = fName;
+    this.department = department;
+    this.administration2 = administration2;
+    this.level = level;
+    this.imageUrl = imageUrl;
+}
